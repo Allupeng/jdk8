f27fc2dc5338bc827e853cf0b7c0621c77721e07
= <=>
2022-04-13 19:33:40 +0800

    初始化jdk8

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author: pengxinru
+ * @description:
+ * @date: 2022/04/13 19:01:20
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
